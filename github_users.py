b6e496678dd96117aa1fb70bff9934e8b90dc554
Peter Han <git@peterhan.me>
2016-09-29 23:27:24 -0700

    json() and pprint() functions

--- github_users.py
+++ github_users.py
@@ -3,6 +3,8 @@
 from __future__ import absolute_import
 import csv
 import sys
+import json
+from pprint import pformat
 from logging import getLogger
 import click
 from github3 import login
@@ -15,6 +17,8 @@ CONTEXT_SETTINGS = {
 
 
 class Application(object):
+    _DEFAULT_FIELD_NAMES = ['name', 'login']
+
     def __init__(self, github_token):
         """
         Log in to GitHub using the given token and grabs the user object of the logged in user
@@ -29,13 +33,17 @@ class Application(object):
         # Grabs the current user
         self._me = self._github.user()
 
-    def _get_users(self, org_name):
+    def _get_users(self, org_name, field_names=_DEFAULT_FIELD_NAMES):
         """
-        Finds one organization with the given name then yields users within the organization.
+        Finds one organization with the given name then yields the given fields of each users in the organization
+        as a list of dictionaries.
         Assumes the given name is exact and there is only 1 organization with the given name.
 
         :param org_name: Name of the organization to look for.
         :type org_name: str
+        :param field_names: List of fields to be written. Refer to https://developer.github.com/v3/users/#response-1 on
+                            the names of the fields. If invalid field is listed, the values will be all empty string.
+        :type field_names: list[str]
         :return: List of users in the given organization
         """
         # Find the organization with the given name
@@ -47,11 +55,12 @@ class Application(object):
             # For every user, grab the whole user object and yield
             # This will cause extra requests and slow things down, but performance for this application
             # is not that important and extra data is always nice
-            yield self._github.user(login=user.login)
+            user = self._github.user(login=user.login)
+            yield dict((field, getattr(user, field, '')) for field in field_names)
 
-    def csv(self, org_name, output=None, field_names=['name', 'login']):
+    def csv(self, org_name, output=None, field_names=_DEFAULT_FIELD_NAMES, *args, **kwargs):
         """
-        Creates a csv file containing all the users in the given organization and write it to output
+        Creates a csv file containing all the users in the given organization
 
         .. seealso:: https://developer.github.com/v3/users/#response-1
 
@@ -76,10 +85,62 @@ class Application(object):
         # The first row is always headers, which are the names of the fields as they are provided
         writer.writeheader()
 
-        for user in self._get_users(org_name=org_name):
-            # Create a dictionary of only requested fields for DictWriter
-            user_dict = dict((field, getattr(user, field, '')) for field in field_names)
-            writer.writerow(user_dict)
+        for user in self._get_users(org_name=org_name, field_names=field_names):
+            writer.writerow(user)
+
+        out_stream.close()
+
+    def json(self, org_name, output=None, field_names=_DEFAULT_FIELD_NAMES, *args, **kwargs):
+        """
+        Creates a json file containing all the users in the given organization
+
+        .. seealso:: https://developer.github.com/v3/users/#response-1
+
+        :param org_name: Name of the GitHub organization whose users will be printed.
+        :type org_name: str
+        :param output: Path to the file that will be written. If the file exists, it will be overwritten, not appended.
+                       If the file does not exist, it will be created.
+                       If set to None, the users will be printed to stdout.
+        :type output: str
+        :param field_names: List of fields to be written. Refer to https://developer.github.com/v3/users/#response-1 on
+                            the names of the fields. If invalid field is listed, the values will be all empty string.
+        :type field_names: list[str]
+        """
+        # GOTCHA: Because stdout is not a file, context manager cannot be used
+        if output is not None:
+            out_stream = open(output, 'w+')
+        else:
+            out_stream = sys.stdout
+
+        json.dump(list(self._get_users(org_name=org_name, field_names=field_names)), out_stream)
+        out_stream.write('\n')
+
+        out_stream.close()
+
+    def pprint(self, org_name, output=None, field_names=_DEFAULT_FIELD_NAMES, *args, **kwargs):
+        """
+        Creates a text file containing all the users in the given organization
+
+        .. seealso:: https://developer.github.com/v3/users/#response-1
+
+        :param org_name: Name of the GitHub organization whose users will be printed.
+        :type org_name: str
+        :param output: Path to the file that will be written. If the file exists, it will be overwritten, not appended.
+                       If the file does not exist, it will be created.
+                       If set to None, the users will be printed to stdout.
+        :type output: str
+        :param field_names: List of fields to be written. Refer to https://developer.github.com/v3/users/#response-1 on
+                            the names of the fields. If invalid field is listed, the values will be all empty string.
+        :type field_names: list[str]
+        """
+        # GOTCHA: Because stdout is not a file, context manager cannot be used
+        if output is not None:
+            out_stream = open(output, 'w+')
+        else:
+            out_stream = sys.stdout
+
+        out_stream.write(pformat(list(self._get_users(org_name=org_name, field_names=field_names))))
+        out_stream.write('\n')
 
         out_stream.close()
 
@@ -95,12 +156,13 @@ class Application(object):
 @click.argument('org_name')
 @click.option(
     '--output-format',
+    # GOTCHA: Because getattr is used, it is very important for the user input to match the function name
     type=click.Choice(['csv', 'json', 'pprint']),
     default='pprint',
     help='The format in which you want the output.',
 )
 @click.option(
-    '--output-file',
+    '--output',
     type=click.Path(),
     default=None,
     help=(
@@ -109,6 +171,7 @@ class Application(object):
         'If not set, the output will be printed to stdout.'
     )
 )
-def main(github_token, org_name, output_format, output_file):
+def main(github_token, org_name, output_format, output):
     app = Application(github_token=github_token)
-    app.csv(org_name=org_name, output=output_file)
+    # Figure out which function to call based on the user input.
+    getattr(app, output_format)(org_name=org_name, output=output)
