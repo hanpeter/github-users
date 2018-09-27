ee9c55f0d9985b47998e0e2b7cf76643498430a2
Peter Han <git@peterhan.me>
2018-09-26 18:29:19 -0700

    Adding dummy unit tests

---
+++ tests/test_application.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+class TestApplication(object):
+
+    def test_init(self):
+        pass
---
+++ tests/test_cli.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+
+class TestCli(object):
+
+    def test_main(self):
+        pass
