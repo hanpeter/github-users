3aa7cf29b2bde2648ee61e40f9db4056be353093
Peter Han <git@peterhan.ca>
2021-08-19 00:06:13 -0700

    Update dependencies (#20)

--- github_users/__version__.py
+++ github_users/__version__.py
@@ -1,4 +1,4 @@
 # -*- coding: utf-8 -*-
 # Follows PEP 440: https://www.python.org/dev/peps/pep-0440/
 # Follows Semantic Versioning: https://semver.org/
-__version__ = '0.4.0'
+__version__ = '0.4.1'
